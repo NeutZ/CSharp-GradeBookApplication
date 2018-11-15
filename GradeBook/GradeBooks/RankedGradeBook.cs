3e7cc2e69b9e4ba9caf0188151ab3506ede50abe
NeutZ <bahancock3@gmail.com>
2018-11-15 23:23:20 +0200

    Completed the module

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+            return 'F';
+        }
+    }
+}
